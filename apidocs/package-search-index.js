5f7f62c094c98d9c80c08f7229640693b278f1b7
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2024-09-15 21:23:20 +0000

    Update site

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.blutorange.maven.plugin.closurecompiler.common"},{"l":"com.github.blutorange.maven.plugin.closurecompiler.plugin"},{"l":"com.github.blutorange.maven.plugin.closurecompiler.shared"}];updateSearchResults();
---
+++ testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.blutorange.maven.plugin.closurecompiler.common"},{"l":"com.github.blutorange.maven.plugin.closurecompiler.test"}];updateSearchResults();
